357801c2fdd67fcec619ff34ad3b1a9b8d1f286b
ntsayzacarias <ntsayyzacarias@gmail.com>
2024-11-23 23:32:31 +0000

    Preparing for Railway deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0', // Allow external connections (required for Railway)
+		port: parseInt(process.env.PORT || '3000', 10), // Use Railway's PORT variable
+	},
 });
