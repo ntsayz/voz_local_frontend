32ed38a570b64d179751c4a2d6ea1c3a263af0f0
ntsayzacarias <ntsayyzacarias@gmail.com>
2024-12-14 04:00:48 +0000

    bug fix on vite config; top level wait permissions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		target: 'esnext', // Ensures compatibility with top-level await
+	  },
 	server: {
 		host: '0.0.0.0', // Allow external connections (required for Railway)
 		port: parseInt(process.env.PORT || '3000', 10), // Use Railway's PORT variable
