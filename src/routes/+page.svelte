3b599e69ea7bbaee820a237c76a690cf5d91c0ce
ntsayzacarias <ntsayyzacarias@gmail.com>
2024-12-14 01:36:02 +0000

    made a discovery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
   import SurveyCard from '$lib/components/survey/Card.svelte';
   import { onMount } from 'svelte';
   import 'aos/dist/aos.css';
+  // @ts-ignore
   import AOS from 'aos';
   
 
@@ -83,23 +84,28 @@
       data-aos-delay="400"
     >
       <!-- Polls Section -->
-      <div class="flex flex-col items-center md:items-start">
-        <img
-          src="graphics/data_trends.svg"
-          alt="Mozambique Community Polls"
-          class="w-128 h-64 mb-4"
-          data-aos="zoom-in"
-        />
-        <h2 
-          class="text-xl font-bold text-brand_grey-800 mb-2"
+      <div 
+          class="group relative p-7 border-gray-200 rounded-lg shadow-md hover:shadow-lg hover:border-primary-100 transition flex items-center justify-center hover:animate-pulse-shadow"
           data-aos="fade-up"
-          data-aos-delay="600"
         >
-        {$t('home.features.polls.title')}
-        </h2>
-        <p class="text-gray-600" data-aos="fade-up" data-aos-delay="800">
-          {$t('home.features.polls.description')}
-        </p>
+        <div class="flex flex-col items-center md:items-start">
+          <img
+            src="graphics/data_trends.svg"
+            alt="Mozambique Community Polls"
+            class="w-128 h-64 mb-4"
+            data-aos="zoom-in"
+          />
+          <h2 
+            class="text-xl font-bold text-brand_grey-800 mb-2"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          >
+          {$t('home.features.polls.title')}
+          </h2>
+          <p class="text-gray-600" data-aos="fade-up" data-aos-delay="800">
+            {$t('home.features.polls.description')}
+          </p>
+        </div>
       </div>
 
       <!-- Reports Section -->
@@ -142,7 +148,7 @@
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
       {#each mockSurveys as survey (survey.id)}
         <div 
-          class="group relative p-3 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg hover:border-primary-500 transition flex items-center justify-center"
+          class="group relative p-3 border-gray-200 rounded-lg shadow-md hover:shadow-lg hover:border-primary-100 transition flex items-center justify-center hover:animate-pulse-shadow"
           data-aos="fade-up"
         >
           <SurveyCard {survey} />
