c8ef287ce89adbd369f7b707318109295f38acb7
Ntsay <ntsayyzacarias@gmail.com>
2024-12-15 21:10:56 +0000

    Changed header background color and opacity

--- src/lib/components/common/Header.svelte
+++ src/lib/components/common/Header.svelte
@@ -65,7 +65,8 @@
 style="
     backdrop-filter: blur(10px);
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    background: rgba(255, 255, 255, 0.2); 
+    background: rgba(14, 30, 46, 0.95);
+
   ">
   <div class="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
     <!-- Logo on the far left -->
