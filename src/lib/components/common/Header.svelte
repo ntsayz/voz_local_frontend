cd6050688ef204d317b7da05473fbc162bd3aa39
Ntsay <ntsayyzacarias@gmail.com>
2024-12-15 20:31:06 +0000

    Added world svg

--- src/lib/components/common/Header.svelte
+++ src/lib/components/common/Header.svelte
@@ -90,12 +90,17 @@
       <button
         aria-haspopup="true"
         aria-expanded={isLocaleDropdownOpen}
-        class="text-sm font-medium text-gray-200 hover:text-blue-400 flex items-center space-x-2"
+        class="flex items-center space-x-2 px-4 py-2"
         on:click={toggleLocaleDropdown}
       >
-        <span>{#if $locale === 'en'}PT{:else}EN{/if}</span>
+        <!-- SVG Image with CSS filter to match text-gray-200 -->
+        <img
+          class="h-4 w-4 filter grayscale brightness-0 invert"
+          src="header/world.svg"
+          alt="World"
+        />
       </button>
-    
+
       {#if isLocaleDropdownOpen}
       <div class="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg z-10">
         {#each languages as lang}
